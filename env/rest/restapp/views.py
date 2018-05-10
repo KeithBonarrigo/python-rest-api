c5e5fd767b3cda1eabe4c861ef32df006e8f0725
keith bonarrigo <keith@keithbonline.com>
2018-05-10 14:37:57 -0700

    adding endpoints

--- env/rest/restapp/views.py
+++ env/rest/restapp/views.py
@@ -2,9 +2,16 @@ from rest_framework import viewsets
 from .serializers import TaskSerializers
 from .models import Task
 
-class TaskViewSet(viewsets.ModelViewSet):
 
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-date_created')
     serializer_class = TaskSerializers
 
-# Create your views here.
+class DueTaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-date_created').filter(completed=False)
+    serializer_class = TaskSerializers
+
+class CompletedViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-date_created').filter(completed=True)
+    serializer_class = TaskSerializers
+
