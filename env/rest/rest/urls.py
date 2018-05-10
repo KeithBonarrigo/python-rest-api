f351c5029df462f6398d65a988e047fd3766804d
keith bonarrigo <keith@keithbonline.com>
2018-05-10 14:57:17 -0700

    adding image upload and media storage

--- env/rest/rest/urls.py
+++ env/rest/rest/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import include, url
 from rest_framework import routers
 from restapp.views import TaskViewSet
 from restapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 #router = routers.DefaultRouter()
 router = routers.SimpleRouter()
@@ -13,4 +15,4 @@ router.register(r'due_task', views.DueTaskViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
