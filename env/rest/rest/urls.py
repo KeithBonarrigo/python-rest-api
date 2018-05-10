0e42762583a95c20d826a7af6cc2bb95eaaf0cbe
keith bonarrigo <keith@keithbonline.com>
2018-05-10 16:09:24 -0700

    adding authentication

--- env/rest/rest/urls.py
+++ env/rest/rest/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.urls import include, url
 from rest_framework import routers
-from restapp.views import TaskViewSet
+from restapp.views import TaskViewSet, CreateUserView
 from restapp import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -13,6 +13,8 @@ router.register(r'task',views.TaskViewSet)
 #router.register(r'due_task', views.DueTaskViewSet)
 
 urlpatterns = [
+    url(r'^register/$', views.CreateUserView.as_view(), name='user'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- env/rest/restapp/views.py
+++ env/rest/restapp/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
-from .serializers import TaskSerializers
+from .serializers import TaskSerializers, UserSerializer
 from .models import Task
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
     queryset = Task.objects.all().order_by('-date_created')
     serializer_class = TaskSerializers
 
@@ -12,6 +16,11 @@ class TaskViewSet(viewsets.ModelViewSet):
     filter_fields = ('completed', )
     ordering = ('-date_created',)
     search_fields = ('task_name', 'task_desc',)
+
+class CreateUserView(CreateAPIView):
+    model = get_user_model()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
 """
 class DueTaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-date_created').filter(completed=False)
